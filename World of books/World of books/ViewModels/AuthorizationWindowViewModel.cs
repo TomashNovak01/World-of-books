279296b35f1cff9a9f49dfdc52c6933e97227eeb
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-05-23 21:58:55 +0500

    Очищения мусор и создание модели-представления для окна AuthorizationWindow.xaml.

---
+++ World of books/World of books/ViewModels/AuthorizationWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using World_of_books.ViewModels.Base;
+
+namespace World_of_books.ViewModels
+{
+    internal class AuthorizationWindowViewModel : ViewModelBase
+    {
+
+    }
+}
