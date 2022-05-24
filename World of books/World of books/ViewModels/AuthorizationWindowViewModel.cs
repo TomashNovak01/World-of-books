8bab13c4fcf0b4a6672d909e792baf520a835ccc
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-05-24 18:14:38 +0500

    Создание команды для открытия окна регистрации.

--- World of books/World of books/ViewModels/AuthorizationWindowViewModel.cs
+++ World of books/World of books/ViewModels/AuthorizationWindowViewModel.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using World_of_books.Infrastructures.Commands;
 using World_of_books.ViewModels.Base;
+using World_of_books.Views.Windows.Authorization;
 
 namespace World_of_books.ViewModels
 {
@@ -14,6 +17,29 @@ namespace World_of_books.ViewModels
         {
             get => _title;
             set => Set(ref _title, value);
+        }
+
+        #region Commands
+
+        #region OpenRegistrationWindowCommand
+        public ICommand OpenRegistrationWindowCommand { get; }
+        private bool _canOpenRegistrationWindowCommandExcute(object p) => true;
+        private void _onOpenRegistrationWindowCommandExcuted(object p)
+        {
+            new RegistrationWindow().Show();
+            new AuthorizationWindow().Close();
+        }
+        #endregion
+
+        #endregion
+
+        public AuthorizationWindowViewModel()
+        {
+            #region Commands
+
+            OpenRegistrationWindowCommand = new LambdaCommand(_onOpenRegistrationWindowCommandExcuted, _canOpenRegistrationWindowCommandExcute);
+
+            #endregion
         }
     }
 }
