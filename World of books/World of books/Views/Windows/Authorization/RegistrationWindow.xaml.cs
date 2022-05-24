76873cefa3b9c7411894f3b72fcfff6929ad951c
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-05-24 18:13:55 +0500

    Создание окна регистрации.

---
+++ World of books/World of books/Views/Windows/Authorization/RegistrationWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace World_of_books.Views.Windows.Authorization
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
