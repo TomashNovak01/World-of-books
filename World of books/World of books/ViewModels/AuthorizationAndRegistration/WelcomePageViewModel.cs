bfa9208c29fc39dcae96fc24886c3709bd30a3b1
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-03 20:17:18 +0500

    Расстановка визуальных элементов на странице добро пожаловать и создание для не модели-представления.

---
+++ World of books/World of books/ViewModels/AuthorizationAndRegistration/WelcomePageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using World_of_books.ViewModels.Base;
+
+namespace World_of_books.ViewModels.AuthorizationAndRegistration
+{
+    internal class WelcomePageViewModel : ViewModelBase
+    {
+        #region Fields
+
+        #region UserName
+        private string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set => Set(ref _userName, value);
+        }
+        #endregion
+
+        #region UserRole
+        private string _userRole;
+        public string UserRole
+        {
+            get => _userRole;
+            set => Set(ref _userRole, value);
+        }
+        #endregion
+
+        #endregion
+
+        public WelcomePageViewModel()
+        {
+
+        }
+    }
+}
