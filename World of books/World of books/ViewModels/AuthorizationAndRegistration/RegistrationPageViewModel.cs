d742c413c87999c111645804770c2e52e4ba4b16
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-11 00:04:43 +0500

    Расстановка визуальных элементов в окне регистрации.

--- World of books/World of books/ViewModels/AuthorizationAndRegistration/RegistrationPageViewModel.cs
+++ World of books/World of books/ViewModels/AuthorizationAndRegistration/RegistrationPageViewModel.cs
@@ -1,22 +1,63 @@
-﻿using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using World_of_books.Infrastructures.Commands;
 using World_of_books.ViewModels.Base;
-using World_of_books.Views.Windows.Authorization;
 
 namespace World_of_books.ViewModels.AuthorizationAndRegistration
 {
     internal class RegistrationPageViewModel : ViewModelBase
     {
         #region Fields
-
-
-        #region AuthorizationPage
-        private Page _authorizationPage = new AuthorizationPage();
-        public Page AuthorizationPage
+        #region LastName
+        private string _lastName;
+        public string LastName
         {
-            get => _authorizationPage;
-            set => Set(ref _authorizationPage, value);
+            get => _lastName;
+            set => Set(ref _lastName, value);
+        }
+        #endregion
+
+        #region FirstName
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set => Set(ref _firstName, value);
+        }
+        #endregion
+
+        #region MiddleName
+        private string _middleName;
+        public string MiddleName
+        {
+            get => _middleName;
+            set => Set(ref _middleName, value);
+        }
+        #endregion
+
+        #region Password
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
+        #endregion
+
+        #region Email
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => Set(ref _email, value);
+        }
+        #endregion
+
+        #region NumberPhone
+        private string _numberPhone;
+        public string NumberPhone
+        {
+            get => _numberPhone;
+            set => Set(ref _numberPhone, value);
         }
         #endregion
         #endregion
@@ -35,10 +76,7 @@ namespace World_of_books.ViewModels.AuthorizationAndRegistration
         #region OpenAuthorizationPageCommand
         public ICommand OpenAuthorizationPageCommand { get; }
         private bool _canOpenAuthorizationPageCommandExcute(object p) => true;
-        private void _onOpenAuthorizationPageCommandExcuted(object p)
-        {
-
-        }
+        private void _onOpenAuthorizationPageCommandExcuted(object p) => AuthorAndRegWindowViewModel.MainFrame.GoBack();
         #endregion
 
         #endregion
