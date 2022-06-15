c5561c10677018a05250ad078f6be63ea9d2dc6a
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-15 19:58:18 +0500

    Добавление контекста у окна клиента.

--- World of books/World of books/ViewModels/Customer/CustomerWindowViewModel.cs
+++ World of books/World of books/ViewModels/Customer/CustomerWindowViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using World_of_books.ViewModels.Base;
 
 namespace World_of_books.ViewModels.Customer
 {
-    internal class CustomerWindowViewModel
+    internal class CustomerWindowViewModel : ViewModelBase
     {
+
     }
 }
