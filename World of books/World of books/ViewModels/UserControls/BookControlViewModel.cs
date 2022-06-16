e9268723c18a578cc3c7ef87a7732cff6c8a7d14
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-16 20:49:21 +0500

    Создание userControl для книг.

---
+++ World of books/World of books/ViewModels/UserControls/BookControlViewModel.cs
@@ -0,0 +1,62 @@
+﻿using World_of_books.Models;
+using World_of_books.ViewModels.Base;
+
+namespace World_of_books.ViewModels.UserControls
+{
+    internal class BookControlViewModel : ViewModelBase
+    {
+        #region Fields
+        private static Book _currentBook;
+
+        #region Title
+        private string _title = _currentBook.Title;
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        #region Summary
+        private string _summary = _currentBook.Summary;
+        public string Summary
+        {
+            get => _summary;
+            set => Set(ref _summary, value);
+        }
+        #endregion
+
+        #region Cost
+        private decimal _cost = _currentBook.Cost;
+        public decimal Cost
+        {
+            get => _cost;
+            set => Set(ref _cost, value);
+        }
+        #endregion
+
+        #region Rating
+        private double _rating = _currentBook.Rating;
+        public double Rating
+        {
+            get => _rating;
+            set => Set(ref _rating, value);
+        }
+        #endregion
+
+        #region PublishHouse
+        private string _publishHouse = _currentBook.PublishingHouse.Title;
+        public string PublishHouse
+        {
+            get => _publishHouse;
+            set => Set(ref _publishHouse, value);
+        }
+        #endregion
+        #endregion
+
+        public BookControlViewModel(Book currentBook)
+        {
+            _currentBook = currentBook;
+        }
+    }
+}
---
+++ World of books/World of books/Views/Windows/UserControls/BookControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace World_of_books.Views.Windows.UserControls
+{
+    public partial class BookControl : UserControl
+    {
+        public BookControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
