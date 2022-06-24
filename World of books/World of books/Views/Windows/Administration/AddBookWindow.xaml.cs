88d1077370d5734c7b2d6e2ebe351d6568b8f047
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-25 02:14:20 +0500

    Добавление окна для добавления книг.

---
+++ World of books/World of books/Views/Windows/Administration/AddBookWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace World_of_books.Views.Windows.Administration
+{
+    public partial class AddBookWindow : Window
+    {
+        public AddBookWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
