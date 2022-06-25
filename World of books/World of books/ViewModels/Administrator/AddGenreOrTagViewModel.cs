349259340f68a1b1799a22d21e1116877a2479e7
TomashNovak <96655294+TomashNovak@users.noreply.github.com>
2022-06-25 13:33:19 +0500

    Разметка и прописывание модели-представления для окна добавления жанров и тегов.

--- World of books/World of books/ViewModels/Administrator/AddGenreOrTagViewModel.cs
+++ World of books/World of books/ViewModels/Administrator/AddGenreOrTagViewModel.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Input;
+using World_of_books.Infrastructures.Commands;
+using World_of_books.Models;
+using World_of_books.ViewModels.Base;
 
 namespace World_of_books.ViewModels.Administrator
 {
-    internal class AddGenreOrTagViewModel
+    internal class AddGenreOrTagViewModel : ViewModelBase
     {
+        #region Fields
+        #region Header
+        private string _header;
+        public string Header
+        {
+            get => _header;
+            set => Set(ref _header, value);
+        }
+        #endregion
+
+        #region Title
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        #region GenreOrTag
+        private int _genreOrTag;
+        public int GenreOrTag
+        {
+            get => _genreOrTag;
+            set => Set(ref _genreOrTag, value);
+        }
+        #endregion
+        #endregion
+
+        public AddGenreOrTagViewModel()
+        {
+            AddItemCommand = new LambdaCommand(_onAddItemCommandExcuted, _canAddItemCommandExcute);
+        }
+
+        #region Commands
+        #region AddItemCommand
+        public ICommand AddItemCommand { get; }
+        private bool _canAddItemCommandExcute(object p) => true;
+        private void _onAddItemCommandExcuted(object p)
+        {
+            if (_genreOrTag != 0 || !string.IsNullOrEmpty(_title))
+            {
+                if (_genreOrTag == 1)
+                    CourseworkEntities.Instance.Genre.Add(new Genre() { Title = _title });
+                else
+                    CourseworkEntities.Instance.Tag.Add(new Tag() { Title = _title });
+
+                CourseworkEntities.Instance.SaveChanges();
+            }
+            else
+                MessageBox.Show("Заполните поля", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        #endregion
+        #endregion
     }
 }
